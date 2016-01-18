64c8b1d3511c92f537e5854be91db00439ad4df0
Layne Penney <layne@laynemobile.com>
2016-01-17 19:19:48 -0600

    begin adding components

--- rxsubscriptions-components/src/test/java/com/laynemobile/rxsubscriptions/ExampleUnitTest.java
+++ rxsubscriptions-components/src/test/java/com/laynemobile/rxsubscriptions/ExampleUnitTest.java
@@ -14,4 +14,18 @@
  * limitations under the License.
  */
 
-include ':rxsubscriptions', ':rxsubscriptions-components'
+package com.laynemobile.rxsubscriptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
